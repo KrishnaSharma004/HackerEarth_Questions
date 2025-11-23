6d73a87866ae1df0b96aa5410a192977a9b60e81
Krishna Sharma <rakesh7355249204@gmail.com>
2025-11-23 15:04:37 +0530

    TheMonkAndClassMarks Using comparator function

--- TheMonkAndClassMarks.cpp
+++ TheMonkAndClassMarks.cpp
@@ -25,3 +25,26 @@ int main(){
     }
     return 0;
 }
+/*USING COMPARATOR FUNCTION*/
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// bool cmp(pair<string,int> a,pair<string,int> b){
+//     if(a.second != b.second){
+//         return a.second > b.second;
+//     }
+//     return a.first < b.first;
+// }
+// int main(){
+//     int n;
+//     cin >> n;
+//     vector<pair<string,int> > v(n);
+//     for(int i=0;i<n;++i){
+//         cin >> v[i].first >> v[i].second;
+//     }
+//     sort(v.begin(),v.end(),cmp);
+//     for(int i=0;i<n;++i){
+//         cout << v[i].first << " " << v[i].second << endl;
+//     }
+//     return 0;
+// }
